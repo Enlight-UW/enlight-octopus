682c3157cac265616bb5c6f28905f4d35cdd2129
cjhall1283 <cjhall1283@gmail.com>
2014-12-13 23:08:01 -0600

    Finished product

--- Octopus/Flashy_Code_ino/Flashy_Code_ino.ino
+++ Octopus/Flashy_Code_ino/Flashy_Code_ino.ino
@@ -22,16 +22,18 @@
 #define LED5g 46
 #define LED6g 48
 
-char string[128];
 int readS;
 int readR;
 int readG;
 int readB;
 int time;
+char command[1024];  
+char commandBuffer[128];  
+int commandBufferSize = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
+  // initialize the digital pins as an output.
   pinMode(LED1r, OUTPUT);
   pinMode(LED1g, OUTPUT);
   pinMode(LED1b, OUTPUT);
@@ -51,48 +53,58 @@ void setup() {
   pinMode(LED6g, OUTPUT);
   pinMode(LED6b, OUTPUT);
 
+
+  int j=1;
+  int k=1;
+
+  for (int i = 1; i<1000; i += k) {
+    k++;
+    setColor(j,(i/4),(i/4),(i/4));
+    setColor((j+3),(i/4),(i/4),(i/4));
+    delay ((8000/i)+100);
+    setColor(j,0,0,0);
+    setColor((j+3),0,0,0);
+    if (j<3) {
+      j++; 
+    } 
+    else {
+      j=1; 
+    }
+  }
+  for (int i = 255; i>0; i-=5) {
+    for (int j = 1; j<7; j++) {
+      setColor(j,i,i,i); 
+    }
+    delay(50);
+  }
+
   BLEMini.begin(57600); 
   Serial.begin(57600);
-  
-readS = 0;
-readR = 0;
-readG = 0;
-readB = 0;
-time = 0;
-  
+
+  readS = 0;
+  readR = 0;
+  readG = 0;
+  readB = 0;
+///  time = 0;
+
 }
 
 void loop(){
 
-  int availableBytes = Serial1.available();
-
-  if (availableBytes > 0){
-    time = 0;
-    for (int i = 0; i< availableByte; i++) {
-      string[i] = Serial1.read();
-      if(string[i] == '*'){
-        
-      //parseStar(string);
-      
-      }
-    }
-    Serial.println(string);
-    
-    //parseString(string);
-    
-//    for (int i = 0; i < sizeof(string); i++){
-//      string[i] = (char)0;
-//    }
+  if (Serial1.available()) {
+    time = 1;
+    readCommand();
+    //    Serial.print(command);
+    parseCommand(command);
+  } 
+  else {
+    time++;
+    delay(100);
   }
-  
-//  while (Serial1.available() < 1 && time < 300){
-//    setColor(readS, readR, readG, readB);
-//    delay(100);
-//    time ++;
-//  }
-//  
-//  setColor(5, 0, 0, 0);
-//  setColor(6, 0, 0, 0);
+
+
+  Serial.println(time);
+
   //   for (int i = 1; i < 7; i++){
   //    setColor(i, 255, 255, 255);
   //  }
@@ -117,7 +129,7 @@ void loop(){
   //  
   //  delay(1000);
   //  
-  //  for (int w = 0; w < 255; w++){
+ //  for (int w = 0; w < 255; w++){
   //    for (int i = 1; i < 7; i++){
   //      setColor(i, 255, w, 255);
   //      delay(2);
@@ -134,41 +146,38 @@ void loop(){
   //  }
   //  
   //  delay (1000);
-  //  
-//  setColor(1, 0, 255, 0);
-//  setColor(3, 0, 255, 0);
-//  setColor(2, 255, 0, 0);
-//  setColor(4, 255, 0, 0);
-//  setColor(5, 255, 255, 255);
-//  setColor(6, 255, 255, 255);
-//
-//  delay(1000);
-//
-//  setColor(2, 0, 255, 0);
-//  setColor(4, 0, 255, 0);
-//  setColor(1, 255, 255, 255);
-//  setColor(3, 225, 255, 255);
-//  setColor(5, 255, 0, 0);
-//  setColor(6, 255, 0, 0);
-//
-//  delay(1000);
-//
-//  setColor(2, 255, 255, 255);
-//  setColor(4, 255, 255, 255);
-//  setColor(1, 255, 255, 255);
-//  setColor(3, 222, 255, 255);
-//  setColor(5, 255, 255, 255);
-//  setColor(6, 255, 255, 255);
-//
-//  delay(100);
+  //
+  if (time>450) {
+    time=301;
+  } 
+  else if(time>400){
+    setColor(1, 0, 255, 0);
+    setColor(4, 0, 255, 0);
+    setColor(2, 255, 0, 0);
+    setColor(5, 255, 0, 0);
+    setColor(3, 255, 255, 255);
+    setColor(6, 255, 255, 255);
+  } 
+  else if (time>350) {
+
+    setColor(2, 0, 255, 0);
+    setColor(5, 0, 255, 0);
+    setColor(1, 255, 255, 255);
+    setColor(4, 225, 255, 255);
+    setColor(3, 255, 0, 0);
+    setColor(6, 255, 0, 0);
+  } 
+  else if (time>300) {
+
+    setColor(2, 255, 255, 255);
+    setColor(5, 255, 255, 255);
+    setColor(1, 255, 0, 0);
+    setColor(4, 222, 0, 0);
+    setColor(3, 0, 255, 0);
+    setColor(6, 0, 255, 0);
+
+  }
 }
-
-
-//void cycle() {
-// setColor(i, 255, 255, 255); 
-//  
-//}
-
 
 void setColor(int n, int r, int g, int b) {
   switch (n) {
@@ -205,29 +214,67 @@ void setColor(int n, int r, int g, int b) {
   }
 
 }
-void parseStar(String string){
-  int startIndex = 0;
- while (string[startIndex] != '*'){  
-  int carrotIndex = string.indexOf('^', startIndex);
-  String command = string.substring(startIndex, carrotIndex);
-  parseCarrot(command);
-  startIndex = carrotIndex+1;
- }
- }  
 
-void parseCarrot(String string){
+void parseCommand(String string){
   int pipeIndex = string.indexOf('|');
   int pipeIndex2 = string.indexOf('|', pipeIndex+1);
   int pipeIndex3 = string.indexOf('|', pipeIndex2+1);
-  
+
   String firstValue = string.substring(0, pipeIndex);
   String secondValue = string.substring(pipeIndex+1, pipeIndex2);
   String thirdValue = string.substring(pipeIndex2+1, pipeIndex3);
   String fourthValue = string.substring(pipeIndex3+1);
-  
+
   readS = firstValue.toInt();
   readR = secondValue.toInt();
   readG = thirdValue.toInt();
   readB = fourthValue.toInt();
   setColor(readS, readR, readG, readB);
 }
+
+// http://blog.ostanin.org/2012/01/08/sending-large-strings-of-data-to-arduino/ 
+
+void readCommandBuffer(int bytesToRead) {  
+  int i = 0;
+  char c = 0;
+  while (i < 128 && (i < bytesToRead || bytesToRead <= 0)) {
+    while (!Serial1.available())
+      ;
+    c = Serial1.read();
+    if (c == '^') {
+      break;
+    }
+    commandBuffer[i] = c;
+    i++;
+  }
+  commandBufferSize = i;
+}
+
+void readCommand() {  
+  command[0] = '\0';
+  readCommandBuffer(0);
+  if (strncmp(commandBuffer, "RCV", 3) == 0) {
+    commandBuffer[commandBufferSize] = '\0';
+    int expectedSize = atoi(commandBuffer + 4);
+    if (expectedSize <= 0 || expectedSize > 1024) {
+      return;
+    }
+    Serial.println("RDY");
+    int bytesRead = 0;
+    while (bytesRead < expectedSize) {
+      readCommandBuffer(expectedSize - bytesRead);
+      memcpy(command + bytesRead, commandBuffer, commandBufferSize);
+      bytesRead += commandBufferSize;
+      Serial.print("ACK ");
+      Serial.println(commandBufferSize);
+    }
+    command[bytesRead] = '\0';
+  } 
+  else {
+    memcpy(command, commandBuffer, commandBufferSize);
+    command[commandBufferSize] = '\0';
+  }
+}
+
+
+
